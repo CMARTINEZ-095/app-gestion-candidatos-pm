815c7ccf8d12b9716b2acf429bb4113a5dbefaa4
CMARTINEZ-095 <Andresmtz095@gmail.com>
2026-03-17 20:59:59 -0500

    (chore):Definicion de rutas principales para la comunicacion de la API generada desde el Back

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+const URL_BASE = 'http://localhost:8080/api';
+export let end_point = {
+    users: URL_BASE + "USERS",
+    offers: URL_BASE + "OFFERS",
+    candidates: URL_BASE + "CANDIDATES",
+}
